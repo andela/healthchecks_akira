2a2cfa050e0c7af4578eeed4e971722c604de335
Maryanne <maryanne.nganga@andela.com>
2017-02-20 20:07:49 +0300

    [Chore #139551321] Modified alice and asserted channel uses pushbullet.

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -6,14 +6,25 @@ class ApiAdminTestCase(BaseTestCase):
 
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
+        self.pwd = 'password'
+
+        # added alice as superuser and as staff.
+        self.alice.is_superuser = True
+        self.alice.is_staff = True
+        self.alice.save()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
-        ### Set Alice to be staff and superuser and save her :)
+        # Set Alice to be staff and superuser and save her :)
 
     def test_it_shows_channel_list_with_pushbullet(self):
-        self.client.login(username="alice@example.org", password="password")
+        self.client.login(username='alice@example.com', password="password")
 
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
 
-        ### Assert for the push bullet
+        # asserted that a new instance of channel is created with the attribute
+        # kind as "pushbullet"
+
+        self.assertEqual(ch.kind, "pushbullet")
+
+        # Assert for the push bullet 
