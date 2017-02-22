f559f8c17c80794756a9646efb1f50b4616eb192
Maryanne <maryanne.nganga@andela.com>
2017-02-22 17:42:14 +0300

    [Chore #139551321] assert for attributes for alice 1 and Alice 2

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -1,5 +1,5 @@
 import json
-from datetime import timedelta as td
+from datetime import datetime, timedelta as td
 from django.utils.timezone import now
 
 from hc.api.models import Check
@@ -40,9 +40,43 @@ class ListChecksTestCase(BaseTestCase):
         self.assertTrue("checks" in doc)
 
         checks = {check["name"]: check for check in doc["checks"]}
+
         ### Assert the expected length of checks
+        self.assertEqual(len(checks), len(doc["checks"]))
         ### Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
         ### last_ping, n_pings and pause_url
+        #import pdb; pdb.set_trace()
+
+        alice1=doc["checks"][1]
+        alice2=doc["checks"][0]
+
+        self.assertEqual(
+                         [
+                         self.a1.timeout.total_seconds(),self.a1.grace.seconds,
+                         self.a1.to_dict()["ping_url"], self.a1.status,
+                         datetime.isoformat(self.a1.last_ping), self.a1.n_pings,
+                         self.a1.to_dict()["pause_url"]
+                         ],
+                         [
+                         alice1["timeout"], alice1["grace"],
+                         alice1["ping_url"], alice1["status"],
+                         alice1["last_ping"], alice1["n_pings"],
+                         alice1["pause_url"]
+                         ])
+
+        self.assertEqual(
+                         [
+                         self.a2.timeout.total_seconds(),self.a2.grace.seconds,
+                         self.a2.to_dict()["ping_url"], self.a2.status,
+                         datetime.isoformat(self.a2.last_ping), self.a2.n_pings,
+                         self.a2.to_dict()["pause_url"]
+                         ],
+                         [
+                         alice2["timeout"], alice2["grace"],
+                         alice2["ping_url"], alice2["status"],
+                         alice2["last_ping"], alice2["n_pings"],
+                         alice2["pause_url"]
+                         ])
 
     def test_it_shows_only_users_checks(self):
         bobs_check = Check(user=self.bob, name="Bob 1")
