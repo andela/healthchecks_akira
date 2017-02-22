8a287bcb8f823104388a1b03830bf0e26bf4b7b5
Denis Gathondu <denis.gathondu@andela.com>
2017-02-22 22:45:14 +0300

    [Chore #139551321] Adds test for new check not being in grace period
    it was initially in the test paused check not in grace period

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -13,9 +13,9 @@ class CheckModelTestCase(TestCase):
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
         check.tags = ""
-        self.assertEquals(check.tags_list(),[])
+        self.assertEquals(check.tags_list(), [])
 
-        ### Repeat above test for when check is an empty string
+        # Repeat above test for when check is an empty string
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -26,7 +26,7 @@ class CheckModelTestCase(TestCase):
         self.assertTrue(check.in_grace_period())
         self.assertEqual(check.get_status(), "up")
 
-        ### The above 2 asserts fail. Make them pass
+        # The above 2 asserts fail. Make them pass
 
     def test_paused_check_is_not_in_grace_period(self):
         check = Check()
@@ -38,7 +38,8 @@ class CheckModelTestCase(TestCase):
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
 
+    # Test that when a new check is created, it is not in the grace period
+    def test_new_check_is_not_in_grace_period(self):
+        check = Check()
         check.status = "new"
         self.assertFalse(check.in_grace_period())
-
-    ### Test that when a new check is created, it is not in the grace period
