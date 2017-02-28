641b7bfa222c28f81273915d0fe71f28fd61429f
Denis Gathondu <thundoss@gmail.com>
2017-02-28 15:39:58 +0300

    Revert "#139551331 User requires a new dashboard tab for unresolved stuff"

--- hc/accounts/urls.py
+++ hc/accounts/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^set_password/([\w-]+)/$',
         views.set_password, name="hc-set-password"),
 
-    url(r'^switch_team/([\w-]+)/$',
+   url(r'^switch_team/([\w-]+)/$',
         views.switch_team, name="hc-switch-team"),
 
 
